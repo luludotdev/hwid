e965141bbea6c987a66093d41bcb076ed13ed956
Jack Baron <jackmbaron@gmail.com>
2019-04-08 02:59:41 +0100

    Add unix based HWID getter

---
+++ src/unix.ts
@@ -0,0 +1,35 @@
+import { exec } from 'child_process'
+const { platform } = process
+
+export const unixHWID: () => Promise<string> = () =>
+  new Promise((resolve, reject) => {
+    const command =
+      platform === 'darwin'
+        ? 'ioreg -rd1 -c IOPlatformExpertDevice'
+        : 'cat /var/lib/dbus/machine-id /etc/machine-id 2> /dev/null'
+
+    exec(command, (err, stdout) => {
+      if (err) return reject(err)
+
+      const parsed = parse(stdout)
+      return resolve(parsed)
+    })
+  })
+
+const parse = (stdout: string) => {
+  switch (platform) {
+    case 'darwin':
+      return stdout
+        .split('IOPlatformUUID')[1]
+        .split('\n')[0]
+        .replace(/\=|\s+|\"/gi, '')
+        .toLowerCase()
+    case 'linux':
+      return stdout
+        .toString()
+        .replace(/\r+|\n+|\s+/gi, '')
+        .toLowerCase()
+    default:
+      return ''
+  }
+}
