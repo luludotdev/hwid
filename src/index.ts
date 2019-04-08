e841d5601d4c6848b225ebd25c30fe65e4cf0d3d
Jack Baron <jackmbaron@gmail.com>
2019-04-08 03:00:22 +0100

    resolve hwid based on platform

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,24 @@
 import { createHash } from 'crypto'
+import { unixHWID } from './unix'
+import { win32HWID } from './win32'
+const { platform } = process
 
 type Algorithm = 'md5' | 'sha1' | 'sha256'
 const hash = (id: string, algo?: Algorithm) => {
   const h = createHash(algo || 'sha256')
   h.update(id)
   return h.digest('hex')
+}
+
+const resolveID = async () => {
+  switch (platform) {
+    case 'win32':
+      return win32HWID()
+    case 'darwin':
+      return unixHWID()
+    case 'linux':
+      return unixHWID()
+    default:
+      return undefined
+  }
 }
