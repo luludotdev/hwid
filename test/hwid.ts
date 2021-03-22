6039f5bdcb3c093c401e7f6d1ee22620a8f86cac
Jack Baron <jackmbaron@gmail.com>
2021-03-22 04:11:06 +0000

    add basic hwid test

---
+++ test/hwid.ts
@@ -0,0 +1,10 @@
+import test from 'ava'
+import { getHWID } from '../src'
+
+test('returns a string with length > 0', async t => {
+  await t.notThrowsAsync(async () => {
+    const hwid = await getHWID()
+    t.is(typeof hwid, 'string')
+    t.assert(hwid.length > 0)
+  })
+})
