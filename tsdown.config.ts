052b004fafe1fde2fca5b8e4d302389309cecdcb
Jack Baron <jackmbaron@gmail.com>
2025-12-18 01:10:28 +0000

    update tsdown

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,34 +1,23 @@
-import { readdir } from "node:fs/promises";
-import { parse } from "node:path";
-import type { UserConfig, UserConfigFn } from "tsdown";
+import type { UserConfig } from "tsdown";
 import { defineConfig } from "tsdown";
 
-const config: UserConfigFn = defineConfig(async () => {
-  const entry: Record<string, string> = {
+const config: UserConfig = defineConfig({
+  entry: {
     index: "./src/index.ts",
     bin: "./src/bin.ts",
-  };
-
-  const platforms = await readdir("./src/platforms");
-  for (const platform of platforms) {
-    const { name } = parse(platform);
-    entry[name] = `./src/platforms/${platform}`;
-  }
-
-  return {
-    entry,
-    exports: {
-      enabled: true,
-      customExports: (pkg, context) => {
-        context.pkg.bin = pkg["./bin"];
-        delete pkg["./bin"];
-        return pkg;
-      },
+    "*": "./src/platforms/*.ts",
+  },
+  exports: {
+    enabled: true,
+    customExports: (pkg, context) => {
+      context.pkg.bin = pkg["./bin"];
+      delete pkg["./bin"];
+      return pkg;
     },
-    platform: "node",
-    tsconfig: true,
-    skipNodeModulesBundle: true,
-  } satisfies UserConfig;
+  },
+  platform: "node",
+  tsconfig: true,
+  skipNodeModulesBundle: true,
 });
 
 export default config;
